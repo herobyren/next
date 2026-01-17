cc70c76ed6a885d1194e42ca2fddab8900462967
herobyren <632797466@qq.com>
2026-01-17 10:31:53 +0800

    引用注释

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // 主程序 (main process)
 // console.log('Hello from Electron 👋');
 
+// app 这个模块控制着您应用程序的事件生命周期。
+// BrowserWindow 这个模块创建和管理 app 的窗口。
 const { app, BrowserWindow } = require('electron');
 
 const createWindow = function () {
