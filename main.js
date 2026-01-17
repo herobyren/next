31d11e7b9b0a1ed7533c85c05735d38ac30b3ca3
herobyren <632797466@qq.com>
2026-01-17 10:29:51 +0800

    更改箭头函数

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 
 const { app, BrowserWindow } = require('electron');
 
-const createWindow = () => {
+const createWindow = function () {
     const win = new BrowserWindow({
         width: 800,
         height: 600
