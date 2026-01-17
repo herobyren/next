d3b34fe0bfa38ae22ba38efa2cdf94acc992e89b
herobyren <632797466@qq.com>
2026-01-17 11:29:03 +0800

    预加载脚本

---
+++ preload.js
@@ -0,0 +1,13 @@
+const { contextBridge } = require('electron')
+// 该脚本通过 versions 这一全局变量，将 Electron 的 process.versions 对象暴露给渲染器。
+contextBridge.exposeInMainWorld('versions', {
+    node: function () {
+        return process.versions.node;
+    },
+    chrome: function () {
+        return process.versions.chrome;
+    },
+    electron: function () {
+        return process.versions.electron;
+    }
+});
